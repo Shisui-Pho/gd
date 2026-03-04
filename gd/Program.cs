da2671e1a3278856a60655ba6d2caee3784d5b48
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2026-03-04 20:45:07 +0200

    DPI implementation

--- gd/Program.cs
+++ gd/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console.Cli;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using GD.Commands;
+using GD.Services;
+using GD.Services.Abstractions;
+using GD.Infrastructure;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+//DPI
+var services = new ServiceCollection();
+services.AddSingleton<IGDListService,GDListService>();
+
+var register = new TypeRegistrar(services);
+
+var app = new CommandApp(register);
+app.Configure(config =>
+{
+    config.AddCommand<ListCommand>("list");
+});
+
+return app.Run(args);
